bf768b81d5860bfe453cb9a04f85cb03e4ec3a31
Wendel Nascimento <wendel.nascimento2@outlook.com>
2020-02-11 00:27:31 -0300

    feat(BaseLayout): adding BaseLayout layout component

---
+++ src/layouts/BaseLayout/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  max-width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: ${({ theme }) => theme.superlightGray};
+`;
