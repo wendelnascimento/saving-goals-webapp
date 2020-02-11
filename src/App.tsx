9dfb8d53869844c1c3c5840f929b9efb68780186
Wendel Nascimento <wendel.nascimento2@outlook.com>
2020-02-11 00:29:28 -0300

    feat: adding styled theme, global styles and main screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import * as React from 'react';
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+import BaseLayout from './layouts/BaseLayout';
+import SavingPlanSimulation from './screens/SavingPlanSimulation/SavingPlanSimulation';
+
+import THEME from './util/theme';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  html, body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Work Sans', sans-serif;
+  }
+`;
 
 const App: React.FunctionComponent = () => {
-  return <div>Origin Frontend Take Home</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={THEME}>
+        <BaseLayout>
+          <SavingPlanSimulation />
+        </BaseLayout>
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default App;
