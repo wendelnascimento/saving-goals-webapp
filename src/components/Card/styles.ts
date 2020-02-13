c7046543bdfb7eab28335b729e47cb472577b594
Wendel Nascimento <wendel.nascimento2@outlook.com>
2020-02-13 13:10:18 -0300

    fix: change Card padding

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const CardContainer = styled.div`
   background: ${({ theme }) => theme.white};
-  padding: 16px;
+  padding: 32px;
   border-radius: 4px;
 `;
